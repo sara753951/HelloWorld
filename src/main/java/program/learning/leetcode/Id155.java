595f3c2e16d5936a344798fbf18cd378579d16fd
yanxinming <yanxinming@jd.com>
2020-06-22 20:39:06 +0800

    id

---
+++ src/main/java/program/learning/leetcode/Id155.java
@@ -0,0 +1,59 @@
+package program.learning.leetcode;
+
+import java.util.Stack;
+
+/**
+ * Created by yanxinming on 2020/6/17
+ */
+public class Id155 {
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());   //--> 返回 -3.
+        minStack.pop();
+        System.out.println(minStack.top());      //--> 返回 0.
+        System.out.println(minStack.getMin());   //--> 返回 -2.
+
+    }
+
+
+}
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            int popData = stack.pop();
+            if (popData == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
