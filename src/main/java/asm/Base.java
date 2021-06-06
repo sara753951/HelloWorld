b5cbb7684b244ac3185cc66e692380d2063c8d80
yanxinming <yanxinming@meituan.com>
2021-06-06 14:25:31 +0800

    asm learning

---
+++ src/main/java/asm/Base.java
@@ -0,0 +1,12 @@
+package asm;
+
+/**
+ * @Auther yanxinming
+ * @Date 2021/6/2
+ * @Description
+ */
+public class Base {
+    public void process(){
+        System.out.println("process");
+    }
+}
---
+++ src/main/java/asm/Test.java
@@ -0,0 +1,13 @@
+package asm;
+
+/**
+ * @Auther yanxinming
+ * @Date 2021/6/2
+ * @Description
+ */
+public class Test {
+    public static void main(String[] args) {
+        Base base = new Base();
+        base.process();
+    }
+}
